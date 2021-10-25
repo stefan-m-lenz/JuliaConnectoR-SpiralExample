f7e47f80aec790c17d9dc450f13f54661b363328
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2021-10-25 11:49:55 +0200

    Added Julia test script for debugging

---
+++ SpiralExample_JuliaTestScript.jl
@@ -0,0 +1,21 @@
+import Pkg
+Pkg.activate("project_1_6")
+Pkg.instantiate()
+include("SpiralExample.jl")
+
+spiraldata = SpiralExample.spiral_samples(nspiral = 100,
+                                           ntotal = 150,
+                                           nsample = 30,
+                                           start = 0.0,
+                                           stop = 6*pi,
+                                           a = 0.0, b = 1.0)
+
+
+model = SpiralExample.LatentTimeSeriesVAE(latent_dim = 4,
+                                           obs_dim = 2,
+                                           rnn_nhidden = 25,
+                                           f_nhidden = 20,
+                                           dec_nhidden = 20)
+epochs = 1
+SpiralExample.train!(model, spiraldata.samp_trajs, spiraldata.samp_ts,
+      epochs = epochs, learningrate = 0.01)
